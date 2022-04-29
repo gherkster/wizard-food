fb1369c2b650f7100008995224a606c5347583f6
Patrick Benter <me@patrickbenter.com>
2022-04-29 22:14:15 +1000

    Create vue form atomic design components

---
+++ frontend/src/components/molecules/TextField.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="form-container">
+    <div class="form-input-container">
+      <div :class="formInputClass">
+        <input-label :label="label" :is-active="isFieldActive" />
+        <label>
+          <input :value="value" :name="path" class="form-input-field" @input="input" @focus="focus" @blur="blur" />
+        </label>
+      </div>
+    </div>
+    <div class="form-validation-message">
+      <validation-message v-show="error">{{ error }}</validation-message>
+    </div>
+  </div>
+</template>
+
+<script>
+import InputLabel from "@/components/atoms/InputLabel";
+import ValidationMessage from "@/components/atoms/ValidationMessage";
+
+export default {
+  name: "TextField",
+  components: { InputLabel, ValidationMessage },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    error: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  data: () => ({
+    isActive: false,
+  }),
+  computed: {
+    isFieldActive: function () {
+      return this.isActive || !!this.value;
+    },
+    formInputClass: function () {
+      return this.error ? "form-input__error" : "form-input";
+    },
+  },
+  methods: {
+    input(event) {
+      this.$emit("input", { path: this.path, value: event.target.value });
+    },
+    focus(event) {
+      this.isActive = true;
+      this.$emit("focus", { path: this.path, value: event.target.value });
+    },
+    blur(event) {
+      this.isActive = false;
+      this.$emit("blur", { path: this.path, value: event.target.value });
+    },
+  },
+};
+</script>
