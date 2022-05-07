2f18a56c7ffc21bab6b01c980809c08ffacdde2a
Patrick Benter <me@patrickbenter.com>
2022-05-07 20:36:54 +1000

    Create icon and rating components for recipe rating

---
+++ frontend/src/components/atoms/Icon.vue
@@ -0,0 +1,37 @@
+<template>
+  <font-awesome-icon
+    :icon="faIcon"
+    :size="size"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+    @click="handleClick"
+  ></font-awesome-icon>
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props: {
+    faIcon: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: false,
+      default: "lg",
+    },
+  },
+  methods: {
+    handleMouseEnter() {
+      this.$emit("mouseenter");
+    },
+    handleMouseLeave() {
+      this.$emit("mouseleave");
+    },
+    handleClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
---
+++ frontend/src/components/molecules/Rating.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="form-input-container form-rating">
+    <icon
+      v-for="(star, index) in length"
+      :fa-icon="
+        index <= currentHighlightedIcon || (currentHighlightedIcon === -1 && index < value) ? 'fa-solid fa-star' : 'fa-regular fa-star'
+      "
+      size="2xl"
+      :key="star"
+      @mouseenter="handleMouseEnter(index)"
+      @mouseleave="handleMouseLeave"
+      @click="handleClick(star)"
+    ></icon>
+  </div>
+</template>
+
+<script>
+import Icon from "@/components/atoms/Icon";
+
+export default {
+  name: "Rating",
+  components: { Icon },
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+    path: {
+      type: String,
+      required: true,
+    },
+    length: {
+      type: Number,
+      required: true,
+    },
+  },
+  mounted() {
+    this.currentHighlightedIcon = this.value;
+  },
+  data: () => ({
+    currentHighlightedIcon: -1,
+  }),
+  methods: {
+    handleMouseEnter(index) {
+      this.currentHighlightedIcon = index;
+    },
+    handleMouseLeave() {
+      this.currentHighlightedIcon = -1;
+    },
+    handleClick(star) {
+      console.log("star rating: ", star);
+      this.$emit("input", { path: this.path, value: star });
+    },
+  },
+};
+</script>
