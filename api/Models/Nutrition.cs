84cd7779cee04cfdafdaabb559d1622164d68bb5
Patrick Benter <me@patrickbenter.com>
2022-12-05 22:28:42 +1100

    Set includes for recipe retrieval methods

--- api/Models/Nutrition.cs
+++ api/Models/Nutrition.cs
@@ -3,8 +3,4 @@ namespace API.Models;
 public class Nutrition
 {
     public decimal? Energy { get; set; }
-    public decimal? Protein { get; set; }
-    public decimal? Carbohydrates { get; set; }
-    public decimal? Fat { get; set; }
-    public decimal? Sodium { get; set; }
 }
