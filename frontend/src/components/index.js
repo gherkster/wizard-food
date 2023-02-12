d11c045b8c5497d85c666efd04fdb868ca57ebac
Patrick Benter <me@patrickbenter.com>
2023-02-12 15:45:51 +1100

    Add button wrapper component to make button with icon consitent, loading etc

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -4,3 +4,4 @@ export { default as XRow } from "./XRow.vue";
 export { default as XInput } from "./XInput.vue";
 export { default as XSelect } from "./XSelect.vue";
 export { default as XUpload } from "./XUpload.vue";
+export { default as XButton } from "./XButton.vue";
