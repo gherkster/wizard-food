19ca58d452a2b85a8a220bb45a66f8ccdd048e24
Patrick Benter <me@patrickbenter.com>
2023-10-13 21:31:58 +1100

    Add directus autocomplete and duration extensions

---
+++ cms/extensions/endpoints/autocomplete/src/index.js
@@ -0,0 +1,39 @@
+/*
+Provide autocomplete functionality for fields within the CMS. Point the autocomplete field to the deployed extension
+location and the function will provide autocomplete results for the /field path.
+
+Results are provided as a list of objects with the key equalling the entered path, and the value being the autocomplete
+value.
+ */
+import { createError } from '@directus/errors';
+
+const MyExtensionError = createError("UNEXPECTED_ERROR", 'Something went wrong...', 500);
+
+export default (router, { services }) => {
+	const { ItemsService } = services;
+
+	router.get('/:field', (req, res, next) => {
+		const recipeService = new ItemsService("recipes", { schema: req.schema, accountability: req.accountability });
+		const field = req.params.field;
+
+		recipeService
+			.readByQuery({
+				sort: [field],
+				fields: [field],
+				filter: {
+					[field]: {
+						_contains: req.query.q
+					},
+					status: {
+						_eq: "published"
+					}
+				}
+			})
+			.then((results) => {
+				return res.json(results.filter(r => !!r[field]));
+			})
+			.catch((error) => {
+				return next(new MyExtensionError());
+			});
+	});
+};
---
+++ cms/extensions/interfaces/duration/src/interface.vue
@@ -0,0 +1,102 @@
+<template>
+  <div v-if="internalDuration" class="custom-duration--container">
+    <v-input
+      v-model="internalDuration.minutes"
+      type="number"
+      suffix="mins"
+      :min="0"
+      hide-arrows
+    />
+    <v-input
+      v-model="internalDuration.hours"
+      type="number"
+      suffix="hrs"
+      :min="0"
+      hide-arrows
+    />
+    <v-input
+      v-model="internalDuration.days"
+      type="number"
+      suffix="days"
+      :min="0"
+      hide-arrows
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref, toRaw, watch } from "vue";
+
+const props = defineProps<{
+  value?: number | string;
+  loading: boolean;
+}>();
+
+watch(
+  () => props.loading,
+  (isLoading, wasLoading) => {
+    if (wasLoading && !isLoading) {
+      // Wait until the component has finished loading, because props.value will not exist until then
+      init();
+    }
+  }
+);
+
+interface Duration {
+  minutes: number;
+  hours: number;
+  days: number;
+}
+
+const internalDuration = ref<Duration | null>(null);
+
+function init() {
+  /**
+   * Remove reactivity so that we can keep a reference to the initial state.
+   * This is needed so the component can emit a numeric value for the database value,
+   * but not have the input values jump around - e.g. entering 61 minutes instantly switching to 1 hour / 1 minute
+   */
+  const initialValue = toRaw(props.value);
+
+  internalDuration.value = initialValue
+    ? convertSecondsToDuration(initialValue)
+    : {
+      minutes: 0,
+      hours: 0,
+      days: 0,
+    };
+
+  const totalDuration = computed<number>(() => {
+    return (
+      internalDuration.value!.days * 24 * 60 * 60 +
+      internalDuration.value!.hours * 60 * 60 +
+      internalDuration.value!.minutes * 60
+    );
+  });
+
+  watch(totalDuration, () => {
+    emit("input", totalDuration.value);
+  });
+}
+
+function convertSecondsToDuration(seconds: number | string) {
+  const secondsNumber =
+    typeof seconds === "string" ? parseInt(seconds, 10) : seconds;
+  return {
+    days: Math.floor(secondsNumber / (3600 * 24)),
+    hours: Math.floor((secondsNumber % (3600 * 24)) / 3600),
+    minutes: Math.floor((secondsNumber % 3600) / 60),
+  };
+}
+
+const emit = defineEmits<{
+  (e: "input", v: number): void;
+}>();
+</script>
+
+<style lang="css" scoped>
+.custom-duration--container {
+  display: flex;
+  column-gap: 24px;
+}
+</style>
