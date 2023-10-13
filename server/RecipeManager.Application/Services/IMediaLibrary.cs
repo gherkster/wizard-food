9dd5e98b42960b6e5cd75866fa4d1c949b388098
Patrick Benter <me@patrickbenter.com>
2023-10-13 21:55:06 +1100

    Move .NET function code to separate branch

--- server/RecipeManager.Application/Services/IMediaLibrary.cs
+++
@@ -1,9 +0,0 @@
-using RecipeManager.Application.Models.Core;
-using RecipeManager.Application.Models.View;
-
-namespace RecipeManager.Application.Services;
-
-public interface IMediaLibrary
-{
-    Task<ImageMeta> ConvertAndStoreImageAsync(byte[] imageBytes, string fileName, string originalContentType);
-}
--- server/RecipeManager.Application/Services/IPersistenceProvider.cs
+++
@@ -1,7 +0,0 @@
-namespace RecipeManager.Application.Services;
-
-public interface IPersistenceProvider
-{
-    Task SaveFileAsync(byte[] fileBytes, string folderPath, string fileName, string contentType);
-    Task DeleteImageAsync(Guid id);
-}
