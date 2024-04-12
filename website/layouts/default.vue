8cc79f9737c9527b1d64052073033e647d1de7d9
Patrick Benter <me@patrickbenter.com>
2024-04-12 17:36:44 +1000

    Update themes and layout

--- website/layouts/default.vue
+++ website/layouts/default.vue
@@ -1,8 +1,34 @@
 <template>
   <div>
-    <div class="nav-header">
-      <recipe-search />
-    </div>
+    <header class="nav-header">
+      <div class="nav-header__links">
+        <span>Home</span>
+        <span>Recipes</span>
+        <span>Categories</span>
+        <span>About</span>
+      </div>
+      <recipe-search class="nav-header__search" />
+    </header>
     <slot />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@use "@/styles/mixins" as m;
+
+.nav-header {
+  display: flex;
+  align-items: center;
+  // TODO Colour variables
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  @include m.spacing("py", "xs");
+  @include m.spacing("px", "sm");
+  &__links {
+    display: flex;
+    @include m.spacing("gx", "sm");
+  }
+  &__search {
+    margin-left: auto;
+  }
+}
+</style>
