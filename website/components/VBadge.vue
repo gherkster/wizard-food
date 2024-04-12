2f8193e6728087aa35686e5eac4e1c9d4943a391
Patrick Benter <me@patrickbenter.com>
2024-04-12 17:38:49 +1000

    Add custom form components

---
+++ website/components/VBadge.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="badge">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+@use "@/styles/variables" as v;
+$badge-size: 18px;
+.badge {
+  width: $badge-size;
+  height: $badge-size;
+  line-height: $badge-size;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 16px;
+  border: v.$border-accent;
+  padding: 4px;
+}
+</style>
