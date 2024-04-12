d816369bb9250213f5f4cabdc84abf0b2a19bd0a
Patrick Benter <me@patrickbenter.com>
2024-04-12 17:38:49 +1000

    Add custom form components

--- website/components/VTag.vue
+++ website/components/VTag.vue
@@ -4,11 +4,19 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
 <style scoped lang="scss">
+@use "@/styles/variables" as v;
+$tag-size: 12px;
 .tag {
-  border: solid red 1px;
+  background-color: v.$colour-primary;
+  border: v.$border-accent;
   padding: 4px 8px;
+
+  height: $tag-size;
+  line-height: $tag-size;
+  border-radius: v.$border-radius-md;
+  text-align: center;
+  font-size: 16px;
+  padding: 8px;
 }
 </style>
