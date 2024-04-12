7d2e0638f9e629da710b5fe92d0c4c4a590e8e0b
Patrick Benter <me@patrickbenter.com>
2024-04-12 17:41:59 +1000

    Split out the servings adjuster component, styling updates

---
+++ website/components/ServingsAdjuster.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="servings-adjuster">
+    <icon name="mdi:minus-circle" :class="{ disabled: servings <= 1 }" @click="decrementServings" />
+    <span>{{ servings }}</span>
+    <icon name="mdi:plus-circle" @click="incrementServings" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  servings: number;
+}>();
+
+const emit = defineEmits<{
+  input: [value: number];
+}>();
+
+function decrementServings() {
+  if (props.servings > 1) {
+    emit("input", props.servings - 1);
+  }
+}
+
+function incrementServings() {
+  emit("input", props.servings + 1);
+}
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/mixins" as m;
+@use "@/styles/variables" as v;
+.servings-adjuster {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  user-select: none;
+  > svg {
+    cursor: pointer;
+    color: v.$colour-active;
+  }
+  span {
+    @include m.spacing("mx", "xxs");
+    text-align: center;
+    min-width: 20px;
+    font-weight: bold;
+  }
+  > a {
+    padding: 4px 8px;
+    // TODO: Use variables font size
+    font-size: 28px;
+  }
+  > .disabled {
+    cursor: not-allowed;
+    opacity: 50%;
+  }
+}
+</style>
--- website/pages/recipes/[slug].vue
+++ website/pages/recipes/[slug].vue
@@ -1,110 +1,111 @@
 <template>
   <div class="page content">
-    <v-row>
-      <v-column col-12 col-lg-12>
-        <div v-if="recipe" class="recipe">
-          <v-row class="wide-gap">
-            <v-column v-if="recipe.coverImage" col-12 col-lg-4>
-              <blurrable-image :img="recipe.coverImage" />
-            </v-column>
-            <v-column col-12 col-lg-8>
-              <v-row>
-                <h1>{{ recipe.title }}</h1>
-              </v-row>
-              <v-row>
-                <div class="recipe__duration">
-                  <span v-if="totalDuration"
-                    >Total <b>{{ totalDuration }}</b></span
-                  >
-                  <span v-if="recipe.preparationDuration"
-                    >Preparation <b>{{ formatMinutesAsDuration(recipe.preparationDuration) }}</b></span
-                  >
-                  <span v-if="recipe.cookingDuration"
-                    >Cooking <b>{{ formatMinutesAsDuration(recipe.cookingDuration) }}</b></span
-                  >
-                  <span v-if="recipe.customDuration"
-                    >{{ recipe.customDurationName }} <b>{{ formatMinutesAsDuration(recipe.customDuration) }}</b></span
-                  >
-                </div>
-              </v-row>
-              <v-row>
-                <div class="recipe__tags">
-                  <v-tag>{{ recipe.category }}</v-tag>
-                  <v-tag>{{ recipe.cuisine }}</v-tag>
-                  <v-tag v-for="tag in recipe.tags" :key="tag">{{ tag }}</v-tag>
-                </div>
-              </v-row>
-            </v-column>
+    <div v-if="recipe" class="recipe">
+      <v-row class="wide-gap">
+        <v-column v-if="recipe.coverImage" col-12 col-lg-4>
+          <blurrable-image :img="recipe.coverImage" />
+        </v-column>
+        <v-column col-12 col-lg-8>
+          <v-row>
+            <h1>{{ recipe.title }}</h1>
           </v-row>
-          <v-row class="wide-gap">
-            <v-column col-12 col-lg-4>
-              <div v-if="recipe.ingredientGroups.length > 0" class="recipe__ingredients">
-                <div class="recipe__ingredients-title">
-                  <h2>Ingredients</h2>
-                  <div class="recipe__multiplier">
-                    <a :class="{ disabled: ingredientMultiplier <= 1 }" @click="decreaseMultiplier">-</a>
-                    <h4>{{ ingredientMultiplier }}</h4>
-                    <a @click="increaseMultiplier">+</a>
-                  </div>
-                </div>
-                <div v-for="ingredientSection in recipe.ingredientGroups" :key="JSON.stringify(ingredientSection)">
-                  <p v-if="ingredientSection.name">
-                    <b>{{ ingredientSection.name }}</b>
-                  </p>
-                  <ul>
-                    <li v-for="ingredient in ingredientSection.ingredients" :key="JSON.stringify(ingredient)">
-                      <span>
-                        {{ adjustIngredientByMultiplier(ingredient.amount) }} {{ ingredient.unit }}
-                        {{ ingredient.name }}</span
-                      >
-                      <span v-if="ingredient.note" class="text-muted"
-                        ><i>&nbsp;{{ ingredient.note }}</i></span
-                      >
-                    </li>
-                  </ul>
-                </div>
+          <v-row>
+            <div class="recipe__duration">
+              <span v-if="totalDuration"
+                >Total <b>{{ totalDuration }}</b></span
+              >
+              <span v-if="recipe.preparationDuration"
+                >Preparation <b>{{ formatMinutesAsDuration(recipe.preparationDuration) }}</b></span
+              >
+              <span v-if="recipe.cookingDuration"
+                >Cooking <b>{{ formatMinutesAsDuration(recipe.cookingDuration) }}</b></span
+              >
+              <span v-if="recipe.customDuration"
+                >{{ recipe.customDurationName }} <b>{{ formatMinutesAsDuration(recipe.customDuration) }}</b></span
+              >
+            </div>
+          </v-row>
+          <v-row>
+            <div class="recipe__tags">
+              <v-tag>{{ recipe.category }}</v-tag>
+              <v-tag>{{ recipe.cuisine }}</v-tag>
+              <v-tag v-for="tag in recipe.tags" :key="tag">{{ tag }}</v-tag>
+            </div>
+          </v-row>
+        </v-column>
+      </v-row>
+      <v-row class="wide-gap">
+        <v-column col-12 col-lg-4>
+          <div v-if="recipe.ingredientGroups.length > 0" class="recipe__ingredients">
+            <div class="recipe__ingredients-title">
+              <h2>Ingredients</h2>
+            </div>
+            <div class="recipe__options">
+              <div class="recipe__multiplier">
+                <span>Servings</span>
+                <servings-adjuster :servings="servings" @input="updateNumberOfServings" />
               </div>
-            </v-column>
-            <v-column col-12 col-lg-8>
-              <div v-if="recipe.instructionGroups.length > 0" class="recipe__instructions">
-                <h2>Instructions</h2>
+              <div class="recipe__units">
+                <v-switch v-model="unitType" :options="['Metric', 'US']" />
+              </div>
+            </div>
+            <v-divider />
+            <div v-for="ingredientSection in recipe.ingredientGroups" :key="JSON.stringify(ingredientSection)">
+              <p v-if="ingredientSection.name">
+                <b>{{ ingredientSection.name }}</b>
+              </p>
+              <ul>
+                <li v-for="ingredient in ingredientSection.ingredients" :key="JSON.stringify(ingredient)">
+                  <span>
+                    {{ adjustIngredientByMultiplier(ingredient.amount) }} {{ ingredient.unit }}
+                    {{ ingredient.name }}</span
+                  >
+                  <span v-if="ingredient.note" class="text-muted"
+                    ><i>&nbsp;{{ ingredient.note }}</i></span
+                  >
+                </li>
+              </ul>
+            </div>
+          </div>
+        </v-column>
+        <v-column col-12 col-lg-8>
+          <div v-if="recipe.instructionGroups.length > 0" class="recipe__instructions">
+            <h2>Instructions</h2>
+            <div
+              v-for="instructionSection in recipe.instructionGroups"
+              :key="JSON.stringify(instructionSection)"
+              class="instruction-section"
+            >
+              <p v-if="instructionSection.name">
+                <b>{{ instructionSection.name }}</b>
+              </p>
+              <div class="instruction-group">
                 <div
-                  v-for="instructionSection in recipe.instructionGroups"
-                  :key="JSON.stringify(instructionSection)"
-                  class="instruction-section"
+                  v-for="(instruction, index) in instructionSection.instructions"
+                  :key="JSON.stringify(instruction)"
+                  class="instruction"
                 >
-                  <p v-if="instructionSection.name">
-                    <b>{{ instructionSection.name }}</b>
-                  </p>
-                  <div class="instruction-group">
-                    <div
-                      v-for="(instruction, index) in instructionSection.instructions"
-                      :key="JSON.stringify(instruction)"
-                      class="instruction"
-                    >
-                      <h4>{{ index + 1 }}.</h4>
-                      <recipe-instruction
-                        :content="instruction.text"
-                        :ingredient-multiplier="ingredientMultiplier"
-                        :original-number-of-servings="originalNumberOfServings"
-                      />
-                      <blurrable-image v-if="instruction.image" :img="instruction.image" />
-                    </div>
-                  </div>
+                  <v-badge>{{ index + 1 }}</v-badge>
+                  <recipe-instruction
+                    :content="instruction.text"
+                    :ingredient-multiplier="servings"
+                    :original-number-of-servings="originalNumberOfServings"
+                  />
+                  <blurrable-image v-if="instruction.image" :img="instruction.image" />
                 </div>
               </div>
-            </v-column>
-          </v-row>
-          <v-row v-if="recipe.note" class="wide-gap">
-            <v-column>
-              <h2>Notes</h2>
-              <div v-html="recipe.note" />
-            </v-column>
-          </v-row>
-        </div>
-      </v-column>
-      <v-column col-12 col-lg-2> </v-column>
-    </v-row>
+            </div>
+          </div>
+        </v-column>
+      </v-row>
+      <v-row v-if="recipe.note" class="wide-gap">
+        <v-column>
+          <h2>Notes</h2>
+          <!-- eslint-disable-next-line vue/no-v-html -->
+          <div v-html="recipe.note" />
+        </v-column>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -128,7 +129,7 @@ const recipesResponse = await useAsyncData(async () => {
     ig.instructions.forEach((i) => {
       if (!i.text) {
         i.html = "";
-        //console.warn("Recipe", recipe.title, "includes an instruction with no value. Instruction: ", i.id);
+        console.warn("Recipe", recipe.title, "includes an instruction with no value. Instruction: ", i.id);
         return;
       }
 
@@ -171,26 +172,21 @@ if (!recipesResponse.data.value) {
 }
 const recipe = ref(RecipeMapper.toClientRecipe(recipesResponse.data.value!));
 
-const ingredientMultiplier = ref<number>(
-  recipe.value.servings && recipe.value.servings > 0 ? recipe.value.servings : 1,
-);
-const originalNumberOfServings = ingredientMultiplier.value;
+const servings = ref<number>(recipe.value.servings && recipe.value.servings > 0 ? recipe.value.servings : 1);
+const originalNumberOfServings = servings.value;
 
 const multipler = useIngredientMultiplier();
 function adjustIngredientByMultiplier(amount?: number) {
   if (!amount) {
     return "";
   }
-  return multipler.multiplyToFraction(amount, ingredientMultiplier.value, originalNumberOfServings);
+  return multipler.multiplyToFraction(amount, servings.value, originalNumberOfServings);
 }
-function increaseMultiplier() {
-  ingredientMultiplier.value++;
+function updateNumberOfServings(newServings: number) {
+  servings.value = newServings;
 }
-function decreaseMultiplier() {
-  if (ingredientMultiplier.value > 1) {
-    ingredientMultiplier.value--;
-  }
-}
+
+const unitType = ref<"Metric" | "US">("Metric");
 
 const totalDuration = computed(() => {
   return formatMinutesAsDuration(
@@ -216,17 +212,60 @@ function formatMinutesAsDuration(seconds: number) {
 </script>
 
 <style lang="scss" scoped>
-@use "../../styles/mixins" as m;
+@use "@/styles/mixins" as m;
+@use "@/styles/variables" as v;
+
+.recipe {
+  &__tags {
+    display: flex;
+    flex-wrap: wrap;
+    @include m.spacing("g", "xs");
+  }
+  &__duration {
+    display: flex;
+    flex-wrap: wrap;
+    @include m.spacing("g", "xs");
+  }
+  &__ingredients {
+    display: flex;
+    flex-direction: column;
+    background-color: v.$colour-primary;
+    border-radius: v.$border-radius-sm;
+    @include m.spacing("p", "sm");
+    &-title {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  }
+  &__instructions {
+    @include m.spacing("py", "sm");
+  }
+  &__options {
+    display: flex;
+    justify-content: space-between;
+    align-items: end;
+  }
+  &__multiplier {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @include m.spacing("gy", "xxs");
+  }
+  ul,
+  ol {
+    @include m.spacing("pl", "xs");
+  }
+}
+
 .instruction-group {
   display: flex;
   flex-direction: column;
+  @include m.spacing("gy", "xs");
 }
 .instruction {
   display: flex;
-  @include m.spacing("gx", "sm");
-  > h4 {
-    margin: 0;
-  }
+  @include m.spacing("gx", "xs");
 }
 </style>
 
@@ -235,8 +274,5 @@ function formatMinutesAsDuration(seconds: number) {
   p {
     margin-top: 0;
   }
-}
-.inline-ingredient {
-  font-weight: bold;
 }
 </style>
