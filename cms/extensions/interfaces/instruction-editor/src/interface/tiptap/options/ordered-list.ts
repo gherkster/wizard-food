14f6914cd0d9ca55f7abbe85db8a8a3277656276
Patrick Benter <me@patrickbenter.com>
2024-04-20 14:12:14 +1000

    Move editor interface into subfolder

---
+++ cms/extensions/interfaces/instruction-editor/src/interface/tiptap/options/ordered-list.ts
@@ -0,0 +1,18 @@
+// https://tiptap.dev/api/nodes/ordered-list
+
+import OrderedList from "@tiptap/extension-ordered-list";
+import ListItem from "@tiptap/extension-list-item";
+import customMessages from "../../i18n/custom-messages";
+import type { Editor } from "@tiptap/core";
+import { Tool } from "../../../common/types/tools";
+
+export default {
+  key: "orderedList",
+  name: customMessages.tools.ordered_list,
+  icon: "format_list_numbered",
+  extension: [OrderedList, ListItem],
+  shortcut: ["meta", "shift", "7"],
+  action: (editor: Editor) => editor.chain().focus().toggleOrderedList().run(),
+  disabled: (editor: Editor) => !editor.can().chain().focus().toggleOrderedList().run(),
+  active: (editor: Editor) => editor.isActive("orderedList"),
+} as Tool;
---
+++ cms/extensions/interfaces/instruction-editor/src/interface/tiptap/options/paragraph.ts
@@ -0,0 +1,26 @@
+// https://tiptap.dev/api/nodes/paragraph
+
+import customMessages from "../../i18n/custom-messages";
+import type { Editor } from "@tiptap/core";
+import { Tool } from "../../../common/types/tools";
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    paragraph: {
+      setParagraph: () => ReturnType;
+    };
+  }
+}
+
+export default {
+  key: "paragraph",
+  name: customMessages.tools.paragraph,
+  // Because we want “Paragraph” selectable from the formats menu, we don’t `excludeFromOptions: true`
+  // Already imported
+  extension: [],
+  groups: ["format"],
+  shortcut: ["meta", "alt", "0"],
+  action: (editor: Editor) => editor.chain().focus().setParagraph().run(),
+  disabled: (editor: Editor) => !editor.can().chain().focus().setParagraph().run(),
+  active: (editor: Editor) => editor.isActive("paragraph"),
+} as Tool;
