97e9d6beb672d45a7f94a0c20b513c564f6f35a7
Patrick Benter <me@patrickbenter.com>
2024-04-30 17:53:35 +1000

    Move duration and servings controls into separate card

--- website/components/ServingsAdjuster.vue
+++ website/components/ServingsAdjuster.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="servings-adjuster">
-    <icon name="mdi:minus-circle" size="32" :class="{ disabled: servings <= 1 }" @click="decrementServings" />
-    <span>{{ servings }}</span>
-    <icon name="mdi:plus-circle" size="32" @click="incrementServings" />
+    <div class="servings-adjuster__control">
+      <icon name="mdi:minus" size="36" :class="{ disabled: servings <= 1 }" @click="decrementServings" />
+      <span>{{ servings }}</span>
+      <icon name="mdi:plus" size="36" @click="incrementServings" />
+    </div>
+    <span>{{ label }}</span>
   </div>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{
-  servings: number;
-}>();
+const props = withDefaults(
+  defineProps<{
+    servings: number;
+    label?: string;
+  }>(),
+  {
+    label: "servings",
+  },
+);
 
 const emit = defineEmits<{
   input: [value: number];
@@ -32,21 +41,26 @@ function incrementServings() {
 .servings-adjuster {
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  user-select: none;
-  > svg {
-    cursor: pointer;
-    color: v.$colour-primary;
-  }
-  span {
-    @include m.spacing("mx", "xxs");
-    text-align: center;
-    min-width: 20px;
-    font-weight: v.$font-weight-bold;
-  }
-  > .disabled {
-    cursor: not-allowed;
-    opacity: 50%;
+  @include m.spacing("gx", "xs");
+
+  &__control {
+    display: flex;
+    align-items: center;
+    user-select: none;
+    > svg {
+      cursor: pointer;
+      color: v.$colour-primary;
+    }
+    span {
+      text-align: center;
+      min-width: 20px;
+      font-size: 20px;
+      font-weight: v.$font-weight-bold;
+    }
+    > .disabled {
+      cursor: not-allowed;
+      opacity: 50%;
+    }
   }
 }
 </style>
