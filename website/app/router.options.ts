da626080fdd283aeea80c24a387158cf6056707c
Patrick Benter <me@patrickbenter.com>
2024-05-03 10:49:41 +1000

    Update scroll behaviour to respect saved position

--- website/app/router.options.ts
+++ website/app/router.options.ts
@@ -6,15 +6,24 @@ export default <RouterConfig>{
   1. Navigate to the new page at the current scroll position
   2. Jump to the top, resulting in a flash of content from the middle before the top was displayed
   */
-  scrollBehavior() {
+  scrollBehavior(to, from, savedPosition) {
     const nuxtApp = useNuxtApp();
 
     return new Promise((resolve) => {
       nuxtApp.hooks.hookOnce("page:finish", async () => {
         await nextTick();
+        /*
+        Clicking back/forward in the browser should return to the previous position,
+        while clicking on a link should start at the top
+        */
+        if (savedPosition) {
+          resolve(savedPosition);
+          return;
+        }
+
         resolve({
           top: 0,
-          behavior: "instant",
+          behavior: "auto",
         });
       });
     });
