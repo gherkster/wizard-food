3754b3a57586d69bd672abc526eca43ccc6bf4ce
Patrick Benter <me@patrickbenter.com>
2024-05-05 14:11:48 +1000

    Switch from recipe preview to vcard

--- website/pages/index.vue
+++ website/pages/index.vue
@@ -2,26 +2,60 @@
   <div class="home">
     <section v-if="latestRecipes.length > 0">
       <h2>Latest Recipes</h2>
-      <div class="featured-recipe-list">
-        <recipe-preview v-for="recipe in latestRecipes" :key="recipe.slug" :recipe="recipe" />
+      <div class="recipe-list feature">
+        <v-card
+          v-for="(recipe, index) in latestRecipes"
+          :key="recipe.slug"
+          :title="recipe.title"
+          :description="index === 0 ? recipe.descriptionSnippet : undefined"
+          :link="`/recipes/${recipe.slug}`"
+          :image="recipe.coverImage"
+          :tag="recipe.featuredTag"
+          :duration="recipe.totalDuration"
+          :variant="index === 0 ? 'promo' : 'preview'"
+        />
       </div>
     </section>
     <section v-if="favouriteRecipes.length > 0">
       <h2>Personal Favourites</h2>
-      <div class="featured-recipe-list">
-        <recipe-preview v-for="recipe in favouriteRecipes" :key="recipe.slug" :recipe="recipe" />
+      <div class="recipe-list">
+        <v-card
+          v-for="recipe in favouriteRecipes"
+          :key="recipe.slug"
+          :title="recipe.title"
+          :image="recipe.coverImage"
+          :link="`/recipes/${recipe.slug}`"
+          :tag="recipe.featuredTag"
+          :duration="recipe.totalDuration"
+        />
       </div>
     </section>
     <section v-if="quickRecipes.length > 0">
       <h2>Quick Eats</h2>
-      <div class="featured-recipe-list">
-        <recipe-preview v-for="recipe in quickRecipes" :key="recipe.slug" :recipe="recipe" />
+      <div class="recipe-list">
+        <v-card
+          v-for="recipe in quickRecipes"
+          :key="recipe.slug"
+          :title="recipe.title"
+          :image="recipe.coverImage"
+          :link="`/recipes/${recipe.slug}`"
+          :tag="recipe.featuredTag"
+          :duration="recipe.totalDuration"
+        />
       </div>
     </section>
     <section v-if="worldCuisineRecipes.length > 0">
       <h2>World Cuisines</h2>
-      <div class="featured-recipe-list">
-        <recipe-preview v-for="recipe in worldCuisineRecipes" :key="recipe.slug" :recipe="recipe" />
+      <div class="recipe-list">
+        <v-card
+          v-for="recipe in worldCuisineRecipes"
+          :key="recipe.slug"
+          :title="recipe.title"
+          :image="recipe.coverImage"
+          :link="`/recipes/${recipe.slug}`"
+          :tag="recipe.featuredTag"
+          :duration="recipe.totalDuration"
+        />
       </div>
     </section>
   </div>
@@ -77,7 +111,7 @@ const worldCuisineRecipes = ref(
   display: flex;
   flex-direction: column;
   @include m.spacing("gy", "lg");
-  .featured-recipe-list {
+  .recipe-list {
     display: grid;
     @include m.spacing("g", "sm");
     @include m.breakpoint("xs") {
@@ -96,6 +130,26 @@ const worldCuisineRecipes = ref(
       grid-template-columns: repeat(3, 1fr);
       > *:first-child {
         grid-column: unset;
+      }
+    }
+  }
+  .recipe-list.feature {
+    @include m.breakpoint("xs") {
+      grid-template-columns: repeat(2, 1fr);
+      > *:first-child {
+        grid-column: 1 / 3;
+      }
+    }
+    @include m.breakpoint("sm") {
+      grid-template-columns: repeat(2, 1fr);
+      > *:first-child {
+        grid-column: 1/3;
+      }
+    }
+    @include m.breakpoint("lg") {
+      grid-template-columns: repeat(5, 1fr);
+      > *:first-child {
+        grid-column: 1/4;
       }
     }
   }
