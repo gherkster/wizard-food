77e6efe7a98226457e09fc66fe79a528b9112e57
Patrick Benter <me@patrickbenter.com>
2024-05-08 12:37:17 +1000

    Add search component

---
+++ website/components/VSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <form @submit.prevent="$emit('search', query)">
+    <v-input
+      v-model="query"
+      class="text-muted"
+      icon-left="gravity-ui:magnifier"
+      placeholder="Search.."
+      @update:model-value="$emit('input', query)"
+    />
+  </form>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  value: string;
+}>();
+const query = ref(props.value);
+watch(
+  () => props.value,
+  (newValue) => (query.value = newValue),
+);
+defineEmits<{
+  input: [value: string];
+  search: [value: string];
+}>();
+</script>
---
+++ website/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <nuxt-layout>
+    <div class="center-screen">
+      <h2>{{ errorMessage }}</h2>
+      <v-button size="large" @click="handleError">Home</v-button>
+    </div>
+  </nuxt-layout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+const props = defineProps<{
+  error: NuxtError;
+}>();
+const errorMessage = computed(() =>
+  props.error.statusCode === 404 ? "Page not found! 🙃" : "Something went wrong! 😤",
+);
+// TODO: Does this need to be logged on the server?
+console.error(props.error);
+const handleError = () => clearError({ redirect: "/" });
+</script>
