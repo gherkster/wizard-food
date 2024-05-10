a9a82a4f854808c42465ad18b06dad3af0457839
Patrick Benter <me@patrickbenter.com>
2024-05-10 10:26:32 +1000

    Add favicons

--- website/app.vue
+++ website/app.vue
@@ -4,6 +4,28 @@
   </NuxtLayout>
 </template>
 
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "manifest",
+      href: "/site.webmanifest",
+    },
+  ],
+});
+</script>
+
 <style lang="scss">
 @use "./styles/index.scss";
 </style>
