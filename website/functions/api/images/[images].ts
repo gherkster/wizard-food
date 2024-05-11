5c8e5b53ff915425c9a6c32ecf910cd430b35711
Patrick <patrick@iisys.com.au>
2024-05-11 21:18:49 +1000

    Remove todos that are done or no longer relevant

--- website/functions/api/images/[images].ts
+++ website/functions/api/images/[images].ts
@@ -6,7 +6,6 @@ interface Env {
   CLOUDINARY_API_KEY: string;
 }
 
-// TODO: Make sure images are caching correctly with the modifyDate cache-busting mechanism
 export const onRequestGet: PagesFunction<Env> = async (context) => {
   // TODO: Check in R2 and use that if exists
 
@@ -14,7 +13,6 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
     throw new Error("Cloudinary API key environment variable not defined");
   }
 
-  // TODO: Be able to vary image sizes and create thumbnails
   const imageId = context.params.images;
 
   const { searchParams } = new URL(context.request.url);
@@ -43,7 +41,6 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
     }
   }
 
-  // TODO: Handle no transformations
   const fileName = `v1/recipes/${imageId}.webp`;
   const signingParameters = `${transformations}${fileName}`;
 
@@ -52,15 +49,11 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
   // if not, then get image from cloudinary and cache into r2
   // just follow what https://github.com/kotx/render does for now, performance testing can come later to validate
 
-  // TODO: Make parameters dynamic (but also don't allow abuse through small adjustments)
-  // TODO: Make image extension not needed (or provided in request if required)
-
   // TODO: Directus allows specifying a focal point of the image,
   // use this to make thumbnails/crops focus on the subject properly
   // https://cloudinary.com/documentation/resizing_and_cropping#special_positions
 
   const signature = await generateCloudinaryDeliverySignature(context.env, signingParameters);
-  // TODO: Handle no transformations
   const imageUrl = `https://res.cloudinary.com/dork0lbv9/image/upload/${signature}/${transformations}${fileName}`;
 
   return fetch(imageUrl);
