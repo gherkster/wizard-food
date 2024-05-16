59bdd5b51f3c1456b936b03e4531f0fce141f4fe
Patrick <patrick@iisys.com.au>
2024-05-16 23:09:23 +1000

    Handle singular and plural forms of ingredient units

--- website/composables/useIngredientMultiplier.ts
+++ website/composables/useIngredientMultiplier.ts
@@ -1,10 +1,16 @@
 import Fraction from "fraction.js";
 
+function multiplyToFraction(amount: number, multiplier: number, originalNumberOfServings: number) {
+  return new Fraction(amount).mul(multiplier).div(originalNumberOfServings).toFraction(true);
+}
+
+function multiplyToNumber(amount: number, multiplier: number, originalNumberOfServings: number) {
+  return new Fraction(amount).mul(multiplier).div(originalNumberOfServings).valueOf();
+}
+
 export function useIngredientMultiplier() {
-  function multiplyToFraction(amount: number, multiplier: number, originalNumberOfServings: number) {
-    return new Fraction(amount).mul(multiplier).div(originalNumberOfServings).toFraction(true);
-  }
   return {
     multiplyToFraction,
+    multiplyToNumber,
   };
 }
