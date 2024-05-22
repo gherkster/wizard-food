df64ddf3050ad178ea90c5d918a4061dea135a4e
Patrick Benter <me@patrickbenter.com>
2024-05-22 10:32:29 +1000

    Improve ingredient fraction approximations

--- website/composables/useRecipeFormatter.ts
+++ website/composables/useRecipeFormatter.ts
@@ -11,8 +11,6 @@ function formatIngredient(ingredient: { amount?: number; name: string; unit?: st
   return value.trim();
 }
 
-const smallestFraction = new Fraction("1/8");
-
 /**
  * Format the recipe ingredient in a consistent way
  * @param amount The ingredient amount
@@ -24,19 +22,29 @@ function formatIngredientAmount(amount: number) {
   // Round numbers relative to their size
   if (amount > 1000) {
     amountFraction = amountFraction.div(25).ceil().mul(25);
-  }
-  if (amount > 100) {
+  } else if (amount > 100) {
     amountFraction = amountFraction.div(10).ceil().mul(10);
-  }
-  if (amount > 50) {
+  } else if (amount > 50) {
     amountFraction = amountFraction.div(5).ceil().mul(5);
-  }
-  if (amount > 10) {
+  } else if (amount > 10) {
     amountFraction = amountFraction.round();
+  } else {
+    // @ts-expect-error fraction.js@4.3.7 does not include type for roundTo
+    const roundedToOneEighth = amountFraction.roundTo("1/8") as Fraction;
+    // @ts-expect-error fraction.js@4.3.7 does not include type for roundTo
+    const roundedToOneThird = amountFraction.roundTo("1/3") as Fraction;
+
+    /*
+      We want the maximum accuracy to be 1/8 to avoid excessively precise fractions,
+      however 1/3 or 2/3 is often a better approximation for things like going from 6 to 4 servings
+    */
+    amountFraction =
+      Math.abs(roundedToOneEighth.valueOf() - amount) < Math.abs(roundedToOneThird.valueOf() - amount)
+        ? roundedToOneEighth
+        : roundedToOneThird;
   }
 
-  // @ts-expect-error fraction.js@4.3.7 does not include type for roundTo
-  return amountFraction.roundTo(smallestFraction).toFraction(true).trim();
+  return amountFraction.toFraction(true).trim();
 }
 
 function formatMinutesAsDuration(seconds: number) {
