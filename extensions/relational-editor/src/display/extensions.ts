dff9949d4eede8d2b25cadedca40f94ed51b347e
Patrick Benter <me@patrickbenter.com>
2026-05-02 17:13:13 +1000

    Remove unused dependencies

--- extensions/relational-editor/src/display/extensions.ts
+++ extensions/relational-editor/src/display/extensions.ts
@@ -13,10 +13,6 @@ import ListItem from "@tiptap/extension-list-item";
 import OrderedList from "@tiptap/extension-ordered-list";
 import Paragraph from "@tiptap/extension-paragraph";
 import Strike from "@tiptap/extension-strike";
-import { Table } from "@tiptap/extension-table";
-import { TableCell } from "@tiptap/extension-table-cell";
-import { TableHeader } from "@tiptap/extension-table-header";
-import { TableRow } from "@tiptap/extension-table-row";
 import Text from "@tiptap/extension-text";
 
 import inlineNodeTagNames from "../common/inlineNodeTagNames";
@@ -39,10 +35,6 @@ const extensions = [
   Italic,
   Strike,
   Code,
-  Table,
-  TableHeader,
-  TableRow,
-  TableCell,
 ];
 
 const tipTap = useTipTap();
