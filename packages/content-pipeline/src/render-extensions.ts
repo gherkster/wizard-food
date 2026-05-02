7d485ed22e3c24d8f1de57108b7b7fa2a0a996a4
Patrick Benter <me@patrickbenter.com>
2026-05-02 17:13:13 +1000

    Remove unused dependencies

--- packages/content-pipeline/src/render-extensions.ts
+++ packages/content-pipeline/src/render-extensions.ts
@@ -14,10 +14,6 @@ import ListItem from "@tiptap/extension-list-item";
 import OrderedList from "@tiptap/extension-ordered-list";
 import Paragraph from "@tiptap/extension-paragraph";
 import Strike from "@tiptap/extension-strike";
-import { Table } from "@tiptap/extension-table";
-import { TableCell } from "@tiptap/extension-table-cell";
-import { TableHeader } from "@tiptap/extension-table-header";
-import { TableRow } from "@tiptap/extension-table-row";
 import Text from "@tiptap/extension-text";
 import type {
   EditorDefaultAttributes,
@@ -59,10 +55,6 @@ export const recipeRenderExtensions = [
   Italic,
   Strike,
   Code,
-  Table,
-  TableHeader,
-  TableRow,
-  TableCell,
 ];
 
 type InlineIngredientAttributes = RelationBlockAttrs & {
